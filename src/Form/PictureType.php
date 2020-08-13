055bfb4a407af2dff6dc96b1569b4b424c6c9de7
Alexis CHEMEL <chemel@users.noreply.github.com>
2020-08-13 18:12:51 +0200

    Create new Picture Form

--- src/Form/PictureType.php
+++ src/Form/PictureType.php
@@ -6,14 +6,23 @@ use App\Entity\Picture;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class PictureType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('filename')
-            ->add('title')
+            ->add('title', TextType::class, [
+                'label' => 'picture.form.label.title',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'picture.form.placeholder.title']
+            ])
+            ->add('file', FileType::class, [
+                'label' => 'picture.form.label.file',
+                'attr' => ['class' => 'form-control'],
+                'mapped' => false
+            ])
         ;
     }
 
