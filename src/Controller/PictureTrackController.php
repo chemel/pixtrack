990343913893f5c81e4f7908de1c6cc0495a31c0
Alexis CHEMEL <chemel@users.noreply.github.com>
2020-08-22 17:25:48 +0200

    Picture tracking

--- src/Controller/PictureTrackController.php
+++ src/Controller/PictureTrackController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\PicturePrint;
 use App\Repository\PictureRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,13 +15,23 @@ class PictureTrackController extends AbstractController
     /**
      * @Route("/pix/{filename}", name="picture_track")
      */
-    public function track(string $filename, PictureRepository $pictureRepository)
+    public function track(Request $request, EntityManagerInterface $em, PictureRepository $pictureRepository)
     {
+        $filename = $request->get('filename');
+
         $picture = $pictureRepository->findOneByFilename($filename);
 
         if(!$picture) {
             throw $this->createNotFoundException('Picture not found');
         }
+
+        $print = new PicturePrint();
+        $print->setDate(new \DateTime());
+        $print->setIp($request->getClientIp());
+        $print->setUseragent($request->headers->get('User-Agent'));
+
+        $em->persist($print);
+        $em->flush();
 
         $directory = $this->getParameter('kernel.project_dir') . '/' . $this->getParameter('pictures_directory');
         $file = $directory . '/' . $picture->getFilename();
